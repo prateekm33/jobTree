c639a0e35f4c2a241152c79c00f41580fcd2bf9f
Prateek Madapurmath <prateekm33@gmail.com>
2017-01-20 13:42:20 -0800

    (feat) modifies home component styling

--- client/components/Home/LargeView.jsx
+++ client/components/Home/LargeView.jsx
@@ -8,21 +8,52 @@ export default class LargeView extends React.Component {
     this.showJobs = this.showJobs.bind(this);
   }
 
-  showJobs() {
+  showJobs(evt) {
+    const button = evt.target;
+    button.classList.toggle('active');
     const currDisplay = window.getComputedStyle(this.table).display;
 
     if (currDisplay === 'none') this.table.style.display = 'table';
     else this.table.style.display = 'none';
+
+
   }
 
   render() {
+    // return (
+    //   <div className="company-container">
+    //     <div className="company-name-container">
+    //       <button onClick={this.showJobs} className="company-name btn btn-default">
+    //         {this.props.company.toUpperCase()}
+    //       </button>
+    //       <Summary jobs={this.props.jobs}/>
+    //     </div>
+    //     <table ref={el => this.table = el} className="company-jobs">
+    //       <tbody>
+    //         {
+    //           this.props.jobs.map((job,idx) => (
+    //             <Job key={idx} job={job} type={'table'}/>
+    //           ))
+    //         }
+    //       </tbody>
+    //     </table>
+    //   </div>
+    // )
+
     return (
       <div className="company-container">
-        <div onClick={this.showJobs} className="company-name-container">
-          <div className="company-name">
-            {this.props.company.toUpperCase()}
-          </div>
-        </div>
+        <table className="company-name-container">
+          <tbody>
+            <tr>
+              <td>
+                <button onClick={this.showJobs} className="company-name btn btn-default">
+                  {this.props.company.toUpperCase()}
+                </button>
+              </td>
+              <Summary jobs={this.props.jobs}/>
+            </tr>
+          </tbody>
+        </table>
         <table ref={el => this.table = el} className="company-jobs">
           <tbody>
             {
@@ -35,4 +66,40 @@ export default class LargeView extends React.Component {
       </div>
     )
   }
+}
+
+function Summary(props) {
+  const statuses = [
+    'APPLIED', 'PHONE SCREEN', 'ON SITE', 'REJECTED', 'OFFER'
+  ];
+  const jobs = props.jobs;
+  return (
+    <td className="company-summary">
+      <div className="jobs-count"></div>
+      {
+        displayStats(statusCount(jobs))
+      }
+    </td>
+  )
+}
+
+
+function statusCount(jobs) {
+  return jobs.reduce((count,job) => {
+    count[job.status] = count[job.status] + 1 || 1
+    return count;
+  }, {})
+}
+
+function displayStats(count) {
+  const arr = [];
+  for (let status in count) {
+    arr.push(
+      <div key={status} className={status + "-count"}>
+        {status} : {count[status]}
+      </div>
+    )
+  }
+
+  return arr;
 }
