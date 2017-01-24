6a010d172b1736ec819192001602e1a48069ef48
Prateek Madapurmath <prateekm33@gmail.com>
2017-01-23 16:41:17 -0800

    (feat) makes basic functionality of snakes game

--- client/components/Snakes.jsx
+++ client/components/Snakes.jsx
@@ -20,14 +20,16 @@ export default class Snakes extends React.Component {
       currScore: 0,
       yourBest: 0,
       leader: {user: '', score: 0},
+      length: 31,
+      speed: 1000,
       board: new Array(31).fillWithFn(() => new Array(31).fill(false)),
-      food: {r: 15, c: 25},
+      food: {r: null, c: null},
       tailOnPiece: false,
+      dir: 'right'
     }
 
     this.takenSpots = {},
     this.snake = {head: null, tail: null};
-
     this.handleKeyEvent = this.handleKeyEvent.bind(this);
     this.handleStartClick = this.handleStartClick.bind(this);
     this.startGame = this.startGame.bind(this);
@@ -36,22 +38,20 @@ export default class Snakes extends React.Component {
     this.changeGameDifficulty = this.changeGameDifficulty.bind(this);
     this.summonFood = this.summonFood.bind(this);
     this._moveBody = this._moveBody.bind(this);
+    this.moveSnake = this.moveSnake.bind(this);
+    this.resetGame = this.resetGame.bind(this);
   }
 
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyEvent);
     this.endBtn.disabled = true;
-    this.summonFood();
-
-    const board = this.state.board.map(i => i);
-    const mid = Math.floor(board.length / 2);
-    board[mid][mid] = 1;
-    this.takenSpots[mid] = this.takenSpots[mid] || {};
-    this.takenSpots[mid][mid] = true;
-    this.setState({board});
-    this.snake.head = {r: mid, c: mid, prev: null, next: null};
-    this.snake.tail = this.snake.head;
+    this.resetGame();
   }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyEvent);
+  }
+
   changeGameDifficulty(evt) {
     if (this.state.started) return;
 
@@ -59,17 +59,25 @@ export default class Snakes extends React.Component {
 
     switch (target.id) {
       case 'easy':
-        this.setState({ board: new Array(31).fillWithFn(() => new Array(31).fill(0))})
+        this.resetGame(31, 1000);
       case 'medium':
-        this.setState({ board: new Array(21).fillWithFn(() => new Array(21).fill(0))})
+        this.resetGame(21, 500);
       case 'hard':
-        this.setState({ board: new Array(11).fillWithFn(() => new Array(11).fill(0))})
+        this.resetGame(11, 300);
       default: return;
     }
   }
 
-  resetGame() {
-
+  resetGame(length = this.state.length, speed = this.state.speed) {
+    const board = new Array(length).fillWithFn(() => new Array(length).fill(0));
+    const mid = Math.floor(board.length / 2);
+    board[mid][mid] = 1;
+    this.takenSpots[mid] = this.takenSpots[mid] || {};
+    this.takenSpots[mid][mid] = true;
+    this.setState({board});
+    this.snake.head = {r: mid, c: mid, prev: null, next: null};
+    this.snake.tail = this.snake.head;
+    this.setState({ board, length: length, speed: speed}, this.summonFood);
   }
 
   handleKeyEvent(evt) {
@@ -101,76 +109,81 @@ export default class Snakes extends React.Component {
     }
   }
 
-  startGame(evt, dir = 'right') {
+  temp(moveFn) {
+    clearInterval(this.timer);
+    this.timer = setInterval(moveFn, this.state.speed);
+  }
+
+  startGame(evt, dir) {
     this.endBtn.disabled = false;
     evt.preventDefault();
     this.setState({ started: true});
     const self = this;
 
-    clearInterval(this.timer);
+    this.setState({dir});
 
-    this.timer = setInterval(() => moveSnake(dir), 1000);
+    this.temp(this.moveSnake);
 
-    return function (newDir) {
-      dir = newDir;
+    return function (dir) {
+      this.setState({dir});
     }
+  }
 
-    function moveSnake(dir) {
+  moveSnake(dir = this.state.dir) {
       // triggers a setTimeout that moves the snake in that direction every second or 1.5 seconds (depending on level)
-      const board = self.state.board.map(i => i);
+      const board = this.state.board.map(i => i);
       switch (dir) {
         case 'up':
-          self.wasTailOnPiece();
-          self.isTailOnPiece();
-          self._moveBody();
-          board[self.snake.head.r - 1][self.snake.head.c] = 1;
-          board[self.snake.head.r][self.snake.head.c] = 0;
-          self.snake.head.r = self.snake.head.r - 1;
-          self.takenSpots[self.snake.head.r] = self.takenSpots[self.snake.head.r] || {};
-          self.takenSpots[self.snake.head.r][self.snake.head.c] = true;
-          self.setState({board});
+          this.wasTailOnPiece();
+          this.isTailOnPiece();
+          this._moveBody();
+          board[this.snake.head.r - 1][this.snake.head.c] = 1;
+          board[this.snake.head.r][this.snake.head.c] = 0;
+          this.snake.head.r = this.snake.head.r - 1;
+          this.takenSpots[this.snake.head.r] = this.takenSpots[this.snake.head.r] || {};
+          this.takenSpots[this.snake.head.r][this.snake.head.c] = true;
+          this.setState({board});
           return;
         case 'right':
-          self.wasTailOnPiece();
-          self.isTailOnPiece();
-          self._moveBody();
-          board[self.snake.head.r][self.snake.head.c + 1] = 1;
-          board[self.snake.head.r][self.snake.head.c] = 0;
-          self.snake.head.c = self.snake.head.c + 1;
-          self.takenSpots[self.snake.head.r] = self.takenSpots[self.snake.head.r] || {};
-          self.takenSpots[self.snake.head.r][self.snake.head.c] = true;
-          self.setState({board});
+          this.wasTailOnPiece();
+          this.isTailOnPiece();
+          this._moveBody();
+          board[this.snake.head.r][this.snake.head.c + 1] = 1;
+          board[this.snake.head.r][this.snake.head.c] = 0;
+          this.snake.head.c = this.snake.head.c + 1;
+          this.takenSpots[this.snake.head.r] = this.takenSpots[this.snake.head.r] || {};
+          this.takenSpots[this.snake.head.r][this.snake.head.c] = true;
+          this.setState({board});
           return;
         case 'down':
-          self.wasTailOnPiece();
-          self.isTailOnPiece();
-          self._moveBody();
-          board[self.snake.head.r + 1][self.snake.head.c] = 1;
-          board[self.snake.head.r][self.snake.head.c] = 0;
-          self.snake.head.r = self.snake.head.r + 1;
-          self.takenSpots[self.snake.head.r] = self.takenSpots[self.snake.head.r] || {};
-          self.takenSpots[self.snake.head.r][self.snake.head.c] = true;
-          self.setState({board});
+          this.wasTailOnPiece();
+          this.isTailOnPiece();
+          this._moveBody();
+          board[this.snake.head.r + 1][this.snake.head.c] = 1;
+          board[this.snake.head.r][this.snake.head.c] = 0;
+          this.snake.head.r = this.snake.head.r + 1;
+          this.takenSpots[this.snake.head.r] = this.takenSpots[this.snake.head.r] || {};
+          this.takenSpots[this.snake.head.r][this.snake.head.c] = true;
+          this.setState({board});
           return;
         case 'left':
-          self.wasTailOnPiece();
-          self.isTailOnPiece();
-          self._moveBody();
-          board[self.snake.head.r][self.snake.head.c - 1] = 1;
-          board[self.snake.head.r][self.snake.head.c] = 0;
-          self.snake.head.c = self.snake.head.c - 1;
-          self.takenSpots[self.snake.head.r] = self.takenSpots[self.snake.head.r] || {};
-          self.takenSpots[self.snake.head.r][self.snake.head.c] = true;
-          self.setState({board});
+          this.wasTailOnPiece();
+          this.isTailOnPiece();
+          this._moveBody();
+          board[this.snake.head.r][this.snake.head.c - 1] = 1;
+          board[this.snake.head.r][this.snake.head.c] = 0;
+          this.snake.head.c = this.snake.head.c - 1;
+          this.takenSpots[this.snake.head.r] = this.takenSpots[this.snake.head.r] || {};
+          this.takenSpots[this.snake.head.r][this.snake.head.c] = true;
+          this.setState({board});
           return;
       }
     }
-  }
 
   _moveBody(dir) {
     let node = this.snake.tail;
     this.takenSpots[node.r] = this.takenSpots[node.r] || {};
-    this.takenSpots[node.r][node.c] = false;
+    delete this.takenSpots[node.r][node.c];
     while (node.prev) {
       node.r = node.prev.r;
       node.c = node.prev.c;
@@ -184,8 +197,10 @@ export default class Snakes extends React.Component {
       this.snake.tail = this.snake.tail.next;
       this.takenSpots[this.state.food.r] = this.takenSpots[this.state.food.r] || {};
       this.takenSpots[this.state.food.r][this.state.food.c] = true;
-      this.state.food.r = null;
-      this.state.food.c = null;
+      let newSpeed = this.state.speed - 100;
+      if (newSpeed < 50) newSpeed = 50;
+      this.setState({speed: newSpeed}, () => this.temp(this.moveSnake));
+      this.summonFood();
     } 
   }
 
@@ -221,24 +236,38 @@ export default class Snakes extends React.Component {
     return this.state.board.map((row, r) => (
       <div key={r} className="game-row">
         {
-          row.map((cell, c) => (
-            <div key={c} className={
-              "game-cell" + " " + (
-                ((c === this.state.food.c && r === this.state.food.r) ? 'black' : '') ||
-                ((this.takenSpots[r] && this.takenSpots[r][c]) ? 'black' : '')
-              )
-            }></div>
-          ))
+          row.map((cell, c) => {
+            return (
+              <div key={c} className={"game-cell" + " " +
+                (
+                  (c === this.state.food.c && r === this.state.food.r && this.takenSpots[r] && this.takenSpots[r][c] ? 'green' : '') ||
+                  (c === this.state.food.c && r === this.state.food.r ? 'red' : '') ||
+                  (this.takenSpots[r] && this.takenSpots[r][c] ? 'black' : '')
+                )
+
+              }></div>
+            )
+          })
         }
       </div>
     ))
   }
 
   summonFood() {
-    console.log('TODO --- gen random food piece');
     // choose random row from viable rows
+    const viableSpots = [];
+    this.state.board.forEach((row, r) => {
+      row.forEach((col, c) => {
+        if (this.takenSpots[r] && this.takenSpots[r][c]) return;
+        viableSpots.push(r * this.state.length + c);
+      });
+    });
       // choose random col from viable cols
-
+    const randIdx = Math.floor(Math.random() * viableSpots.length);
+    const randCell = viableSpots[randIdx];
+    const row = Math.floor(randCell / this.state.length);
+    const col = randCell % this.state.length;
+    this.setState({food: {r: row, c: col} });
     // update the board state to mark that cell with a food piece
   }
 
