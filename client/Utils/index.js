61be31c4c034e8244012097f2c4ac69e9c239ec3
Prateek Madapurmath <prateekm33@gmail.com>
2017-01-26 23:37:21 -0800

    (deploy) deploy to prod

--- client/Utils/index.js
+++ client/Utils/index.js
@@ -5,31 +5,32 @@ export const initComponent = (nextState, replace, done) => {
   document.body.scrollTop = 0;
   const path = nextState.location.pathname;
   const user = store.getState().user;
-  if (user) {
-    if (path === '/') replace('/home');
-    return done();
-  }
-  else fetch('/auth/validate', {
-          method: 'get',
-          credentials: 'include'
-        })
-          .then(r => r.json())
-          .then(user => {
-            if (!user) {
-              switch (path) {
-                case '/home':
-                case '/profile':
-                case '/manage': replace('/login');
-                default: break;
-              }
-            }
-            else {
-              store.dispatch(actions.logInUser(user));
-              if (path === '/') replace('/home');
-            }
-            done();
-          })
-          .catch(err => { store.dispatch(actions.asyncError(err))});
+  done();
+  // if (user) {
+  //   if (path === '/') replace('/home');
+  //   return done();
+  // }
+  // else fetch('/auth/validate', {
+  //         method: 'get',
+  //         credentials: 'include'
+  //       })
+  //         .then(r => r.json())
+  //         .then(user => {
+  //           if (!user) {
+  //             switch (path) {
+  //               case '/home':
+  //               case '/profile':
+  //               case '/manage': replace('/login');
+  //               default: break;
+  //             }
+  //           }
+  //           else {
+  //             store.dispatch(actions.logInUser(user));
+  //             if (path === '/') replace('/home');
+  //           }
+  //           done();
+  //         })
+  //         .catch(err => { store.dispatch(actions.asyncError(err))});
 }
 
 export class Queue {
